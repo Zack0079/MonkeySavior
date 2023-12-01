0c121432658dea7cdfd3bb44142d5a431cc60850
micheldaoud <michel_daoud@hotmail.com>
2023-11-30 19:12:22 -0500

    add instructions functionality with TAB

---
+++ Assets/Scripts/GameScene/InstructionsController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class InstructionsController : MonoBehaviour
+{
+    public GameObject instructionsPanel; // Reference to the instructions panel
+
+    void Start()
+    {
+        HideInstructions();
+    }
+
+    void Update()
+    {
+        // Show the instructions panel when TAB is held down
+        if (Input.GetKey(KeyCode.Tab))
+        {
+            ShowInstructions();
+        }
+
+        // Hide the instructions panel when TAB is released
+        if (Input.GetKeyUp(KeyCode.Tab))
+        {
+            HideInstructions();
+        }
+    }
+
+    void ShowInstructions()
+    {
+        instructionsPanel.SetActive(true);
+        Time.timeScale = 0; // Pause the game
+    }
+
+    void HideInstructions()
+    {
+        instructionsPanel.SetActive(false);
+        Time.timeScale = 1; // Resume the game
+    }
+}
