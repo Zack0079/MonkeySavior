550084acb23bab8933d18aed46b08f5ecc632631
Zack0079 <113079573+Zack0079@users.noreply.github.com>
2023-12-03 01:14:14 -0500

    Modifed the health item spawn function

--- Assets/Scripts/GameScene/Misc/PlayerRelated/HealthController.cs
+++ Assets/Scripts/GameScene/Misc/PlayerRelated/HealthController.cs
@@ -12,28 +12,65 @@ public class HealthController : MonoBehaviour
     private GameObject ground;
     private GameObject player;
     public GameObject gameOverCanvas;
-    
+
+    //spawn health pack neear the player
+    public float radius = 10.0f;
+    public float spawnDelay = 5f;
+
     // create a ui for health
     public TextMeshProUGUI healthText;
-    
-    
+
+    private Bounds bounds;
+    public int generatedItem = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player");
-        
+
         healthText.text = "Health: " + health;
-        
+
         //Get the ground object from scene and spawn items on it within its area
         ground = GameObject.Find("Ground");
-        Bounds bounds = ground.GetComponent<MeshRenderer>().bounds;
-        
-        for (int i = 0; i < item_amount; i++)
+        bounds = ground.GetComponent<MeshRenderer>().bounds;
+
+        InvokeRepeating("SpawnHealthPickUp", 0f, spawnDelay);
+
+    }
+
+    void SpawnHealthPickUp()
+    {
+        if ( generatedItem < item_amount)
         {
             //find random position within bounds of ground
-            Vector3 spawnPosition = new Vector3( Random.Range(bounds.min.x, bounds.max.x), bounds.center.y + 1, Random.Range(bounds.min.z, bounds.max.z) );
+            // Vector3 spawnPosition = new Vector3( Random.Range(bounds.min.x, bounds.max.x), bounds.center.y + 1, Random.Range(bounds.min.z, bounds.max.z) );
+            float distance = Random.Range(1f, radius);
+            float angle = Random.Range(0f, Mathf.PI * 2);
+            Vector3 spawnPosition = player.transform.position + new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle)) * distance;
+
+            spawnPosition.y = 1f;
+
+            if (bounds.min.x > spawnPosition.x)
+            {
+                spawnPosition.x = bounds.min.x;
+            }
+            else if (bounds.max.x < spawnPosition.x)
+            {
+                spawnPosition.x = bounds.max.x;
+            }
+
+            if (bounds.min.z > spawnPosition.z)
+            {
+                spawnPosition.z = bounds.min.z;
+            }
+            else if (bounds.max.z < spawnPosition.z)
+            {
+                spawnPosition.z = bounds.max.z;
+            }
+
+
             Instantiate(healthPickupPrefab, spawnPosition, Quaternion.identity);
+            generatedItem+=1;
         }
     }
 
@@ -45,7 +82,7 @@ public class HealthController : MonoBehaviour
         {
             player.SetActive(false);
             gameOverCanvas.SetActive(true);
-            
+
         }
     }
 
@@ -57,6 +94,7 @@ public class HealthController : MonoBehaviour
     public void AddHealth(int amount)
     {
         health += amount;
+        generatedItem-=1;
     }
 
 }
